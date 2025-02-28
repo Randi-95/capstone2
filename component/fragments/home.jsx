458e3c4f2d23ecf757429f04a40d0f01d8c57096
Randi-95 <randiganteng9528@gmail.com>
2025-02-28 10:36:37 +0700

    ganti warna

--- component/fragments/home.jsx
+++ component/fragments/home.jsx
@@ -6,17 +6,17 @@ export const Home = (props) => {
     return (
         <div className="home w-full h-screen flex flex-col items-center justify-center">
             <div className="title">
-                <button className="bg-white py-1 px-2 border border-[#4CB92D] rounded-[20px]"><p className="text-[#4CB92D] font-[600] py-1 px-3 bg-gray-100 rounded-[20px]">Best Financial Tracker</p></button>
+                <button className="bg-white py-1 px-2 border border-[#7f5efd] rounded-[20px]"><p className="text-[#7f5efd] font-[600] py-1 px-3 bg-gray-100 rounded-[20px]">Best Financial Tracker</p></button>
             </div>   
             <div className="content flex flex-col justify-center ">
-                <p className="text-[60px] leading-[65px]  text-center  font-[600] text-[#374151]"><span className="text-[#4CB92D]">Kelola Keuangan dengan</span> <br /> Cerdas Bersama Financial Tracker </p>
+                <p className="text-[60px] leading-[65px]  text-center  font-[600] text-[#374151]"><span className="text-[#7f5efd]">Kelola Keuangan dengan</span> <br /> Cerdas Bersama Financial Tracker </p>
                 <span className="flex justify-center mt-[10px]">
                     <p className="w-[70%] font-[600] text-center text-[#7F7F7F]">Smart Financial Tracker membantu Anda mencatat transaksi, menganalisis pola keuangan, dan memprediksi keuangan dengan Al. 
                     Aman, mudah, dan otomatis!</p>
                 </span>
             </div>
             <div className="mt-[10px]">
-                <Button bgColor='bg-[#4CB92D]' border='border-[#4CB92D]' hover='hover:bg-transparent' hoverText = 'group-hover:text-[#4CB92D] ' >Bergabung Sekarang</Button>
+                <Button bgColor='bg-[#7f5efd]' border='border-[#7f5efd]' hover='hover:bg-transparent' hoverText = 'group-hover:text-[#4CB92D] ' >Bergabung Sekarang</Button>
             </div>
         </div>
     )
--- component/fragments/navbar.jsx
+++ component/fragments/navbar.jsx
@@ -24,19 +24,19 @@ function Navbar() {
         <div className={`navbar fixed top-0 right-0 left-0 transition-all duration-[0.1s] ${scrolling ? "bg-white shadow-lg px-[12px] py-[20px]" : "bg-white px-[25px] py-[30px]"}`}>
             <div className="container-navbar flex justify-between items-center">
                 <div className="nav-logo">
-                    <h2 className="text-[20px] text-[#4CB92D] font-[700]">FinYouth</h2>
+                    <h2 className="text-[20px] text-[#7f5efd] font-[700]">FinYouth</h2>
                 </div>
                 <div className="navbar-nav hidden md:block">
                     <ul className="flex gap-4">
-                        <li><a href=""><p className="text-[#828282] text-[16px] font-[500] hover:text-[#4CB92D]">Beranda</p></a></li>
-                        <li><a href=""><p className="text-[#828282] text-[16px] font-[500] hover:text-[#4CB92D]">Tentang Kami</p></a></li>
-                        <li><a href=""><p className="text-[#828282] text-[16px] font-[500] hover:text-[#4CB92D]">Fitur</p></a></li>
-                        <li><a href=""><p className="text-[#828282] text-[16px] font-[500] hover:text-[#4CB92D]">Kontak</p></a></li>
+                        <li><a href=""><p className="text-[#828282] text-[16px] font-[500] hover:text-[#7f5efd]">Beranda</p></a></li>
+                        <li><a href=""><p className="text-[#828282] text-[16px] font-[500] hover:text-[#7f5efd]">Tentang Kami</p></a></li>
+                        <li><a href=""><p className="text-[#828282] text-[16px] font-[500] hover:text-[#7f5efd]">Fitur</p></a></li>
+                        <li><a href=""><p className="text-[#828282] text-[16px] font-[500] hover:text-[#7f5efd]">Kontak</p></a></li>
                     </ul>
                 </div>
                 <div className="navbar-button flex gap-[5px]">
-                    <Button bgColor='bg-[#4CB92D]' border='border-[#4CB92D]' hover='hover:bg-transparent' hoverText = 'group-hover:text-[#4CB92D] ' >Masuk</Button>
-                    <Button bgColor='bg-transparent' border='border-[#4CB92D]' text='text-[#4CB92D]' hover="hover:bg-[#4CB92D]" hoverText="group-hover:text-white">Daftar</Button>
+                    <Button bgColor='bg-[#7f5efd]' border='border-[#7f5efd]' hover='hover:bg-transparent' hoverText = 'group-hover:text-[#4CB92D] ' >Masuk</Button>
+                    <Button bgColor='bg-transparent' border='border-[#7f5efd]' text='text-[#7f5efd]' hover="hover:bg-[#4CB92D]" hoverText="group-hover:text-white">Daftar</Button>
                 </div>
             </div>
         </div>
