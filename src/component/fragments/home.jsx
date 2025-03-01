1e922a1ca1b4e9bcbe4a6b36d3f6355c936c85b4
Randi-95 <randiganteng9528@gmail.com>
2025-03-02 01:17:54 +0700

    hero

--- src/component/fragments/home.jsx
+++ src/component/fragments/home.jsx
@@ -9,9 +9,9 @@ export const Home = (props) => {
                 <button className="dark:bg-transparent bg-white py-1 px-2 border border-primary rounded-[20px]"><p className="dark:bg-indigo-900 dark:opacity-50 text-primary font-[600] py-1 px-3 bg-gray-100 rounded-[20px]">Best Financial Tracker</p></button>
             </div>   
             <div className="content flex flex-col justify-center">
-                <p className="dark:text-gray-300 lg:text-[60px] md:text-[50px] text-[30px] sm:text-[40px] leading-[40px] md:leading-[65px]  text-center  font-[600] text-[#374151] px-[14px]"><span className="text-primary">{header}</span> <br /> {isi} </p>
+                <p className="dark:text-gray-300 lg:text-[60px] md:text-[50px] text-[30px] sm:text-[40px] leading-[40px] md:leading-[65px]  text-center  font-[600] text-[#374151]"><span className="text-primary">{header}</span> <br /> {isi} </p>
                 <span className="flex justify-center mt-[10px]">
-                    <p className="w-[70%] font-[600] text-center text-[#7F7F7F]">{deskripsi}</p>
+                    <p className="w-[80%] font-[600] text-center text-[#7F7F7F]">{deskripsi}</p>
                 </span>
             </div>
             <div className="mt-[10px]">
--- src/pages/TentangKami.jsx
+++ src/pages/TentangKami.jsx
@@ -6,8 +6,8 @@ function TentangKamiPage() {
 
   return (
       <div className="">
-        <Navbar type = 'tentangkami'/>
-        <Home header = 'Kenali Lebih Dekat Financial Tracker' isi = 'Solusi Keuangan Pintar untuk Masa Depan Anda' deskripsi = 'Financial Tracker hadir untuk membantu Anda mengelola keuangan dengan lebih baik. Dengan teknologi AI, kami memberikan solusi pencatatan transaksi, analisis keuangan, dan prediksi keuangan yang aman dan otomatis. Bergabunglah dan kelola keuangan Anda dengan lebih cerdas!'/>
+        <Navbar/>
+        <Home header = 'Mengenal Financial Tracker' isi = 'Solusi Keuangan Pintar' deskripsi = 'Financial Tracker hadir untuk membantu Anda mengelola keuangan dengan lebih baik.'/>
       </div>
   )
 }
