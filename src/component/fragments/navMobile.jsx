62b913d3780378d05c86f21a7c34abb57736f96b
Randi-95 <randiganteng9528@gmail.com>
2025-03-25 14:02:35 +0700

    navbar mobile

--- src/component/fragments/navMobile.jsx
+++ src/component/fragments/navMobile.jsx
@@ -4,18 +4,18 @@ import { Calendar, BarChart, Grid, MessageCircle,Menu, DollarSign, Clock, Activi
 function NavMobile() {
     return (
         <div className="nav-mobile fixed bottom-0 right-0 left-0">
-            <div className="border-t-[1.4px] border-[#d9d7d7] w-full py-4 flex justify-between p-2 px-4">
+            <div className=" shadow-[0_-8px_10px_-1px_rgba(0,0,0,0.1)] w-full py-4 flex justify-between p-2 px-4">
                 <ul >
-                    <li className="font-bold text-primary text-md flex flex-col items-center"><Home className="text-primary"/>Beranda</li>
+                    <li className="font-bold text-primary text-xs flex flex-col items-center"><Home className="text-primary"/>Beranda</li>
                 </ul>
                 <ul>
-                    <li className="font-semibold text-md flex flex-col items-center"><BarChart className=""/>Grafik</li>
+                    <li className=" text-xs flex flex-col items-center"><BarChart className=""/>Grafik</li>
                 </ul>
                 <ul>
-                    <li className="font-semibold text-md flex flex-col items-center"><Activity className=""/>Prediksi</li>
+                    <li className=" text-xs flex flex-col items-center"><Activity className=""/>Prediksi</li>
                 </ul>
                 <ul>
-                    <li className="font-semibold text-md flex flex-col items-center"><MessageCircle className=""/>Chatbot</li>
+                    <li className=" text-xs flex flex-col items-center"><MessageCircle className=""/>Chatbot</li>
                 </ul>
             </div>
         </div>
