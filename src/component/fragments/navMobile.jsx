99efe9a8e8de206ac12fb6001e98e016121c04d8
Randi-95 <randiganteng9528@gmail.com>
2025-03-25 11:41:51 +0700

    border

--- src/component/fragments/navMobile.jsx
+++ src/component/fragments/navMobile.jsx
@@ -4,7 +4,7 @@ import { Calendar, BarChart, Grid, MessageCircle,Menu, DollarSign, Clock, Activi
 function NavMobile() {
     return (
         <div className="nav-mobile fixed bottom-0 right-0 left-0">
-            <div className="border-t border-[#a09999] w-full py-4 flex justify-between p-2 px-4">
+            <div className="border-t-[1.4px] border-[#d9d7d7] w-full py-4 flex justify-between p-2 px-4">
                 <ul >
                     <li className="font-bold text-primary text-md flex flex-col items-center"><Home className="text-primary"/>Beranda</li>
                 </ul>
